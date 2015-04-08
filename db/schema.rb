977021dcc11b07db9daccbc16fa068aa7702543e
Ivan <psyxoz228@gmail.com>
2015-04-08 15:55:26 +0300

    continuous learn engine(app)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407131202) do
+ActiveRecord::Schema.define(version: 20150408061014) do
 
   create_table "blorgh_comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150407131202) do
   create_table "blorgh_posts", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
